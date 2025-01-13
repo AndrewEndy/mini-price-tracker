e1440d571a16aa26b7c95c5852ff96fdd6448d99
Andrew <andrijpastuh@gmail.com>
2025-01-13 19:29:45 +0200

    Change files
    - Add the ability to work with the Yabko store

--- app/checks/check_url.py
+++ app/checks/check_url.py
@@ -6,6 +6,7 @@ from app.checks.check_willmax import is_url_for_product_willmax, is_url_for_will
 from app.checks.check_rozetka import is_url_for_product_rozetka, is_url_for_rozetka
 from app.checks.check_avrora import is_url_for_product_avrora, is_url_for_avrora
 from app.checks.check_epicentr import is_url_for_product_epicentr, is_url_for_epicentr
+from app.checks.check_yabko import is_url_for_product_yabko, is_url_for_yabko
 
 
 
@@ -49,6 +50,11 @@ async def check_desired_site(url: str, store_name: str) -> bool:
     if store_name == 'epicentr':
         if not is_url_for_epicentr(url): return False
         flag = await is_url_for_product_epicentr(url)
+        
+        
+    if store_name == 'yabko':
+        if not is_url_for_yabko(url): return False
+        flag = await is_url_for_product_yabko(url)
     
     
     return flag
