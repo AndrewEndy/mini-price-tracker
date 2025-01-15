8547586af087adab7f13b602e0947de6ede5b09e
Andrew <andrijpastuh@gmail.com>
2025-01-15 23:33:18 +0200

    Change staff_parser
    Change the structure of parsing, now everything works more separately from each other
    
    Add a new function get_parsed_changes_staff for correct operation of asynchronous parsing

--- app/parser/staff_parser.py
+++ app/parser/staff_parser.py
@@ -9,61 +9,73 @@ from app.db import SessionLocal
 
 
 class StaffParser(BaseParser):
-    async def parse(self, url: str, tg_id: int) -> Tuple['Product', 'Price'] | None:
-        html_content = await self.fetch_page(url)
+    async def parse(self, html_content, url: str, tg_id: int) -> Tuple['Product', 'Price'] | None:
+        soup = BeautifulSoup(html_content, 'html.parser')
         
-        if html_content:
-            soup = BeautifulSoup(html_content, 'html.parser')
-            
-            product_name = soup.find('div', class_='product__title').find('h1').text.strip()
-            
-            # Пошук статуса товара
-            if soup.find('div', class_='product__ends-container'):
-                status = soup.find('div', class_='product__ends').text.strip() 
-            else:
-                status = 'Є в наявності'
-                
-            # Пошук ціни, знижки, валюти
-            price_block = soup.find('div', class_='product__price')
-            
-            discount = True if price_block.find('span', class_='product__price--old') else False
-            price = float(price_block.text.strip().split()[0])
-            currency = price_block.text.strip().split()[-1].replace('.','')
-            
-            
-            # print('--->' + product_name + '<---')
-            # print('--->' + str(status) + '<---')
-            # print('--->' + str(discount) + '<---')
-            # print('--->' + currency + '<---')
-            # print('--->' + str(price) + '<---')
-            
-            
-            price_obj = Price(price=price, currency=currency, discount=discount, status=status, unit_of_measure=None)
-            product_obj = Product(product_name=product_name, store_name='Staff', url=url, tg_id=tg_id)
-            
-            # print(f'{product_obj}\n\n{price_obj}')
-            
-            # async with SessionLocal() as session:
-            #     session.add(product)
-            #     await session.commit()
-                
-            return product_obj, price_obj
+        product_name = soup.find('div', class_='product__title').find('h1').text.strip()
+        
+        # Пошук статуса товара
+        if soup.find('div', class_='product__ends-container'):
+            status = soup.find('div', class_='product__ends').text.strip() 
         else:
-            return None
+            status = 'Є в наявності'
+            
+        # Пошук ціни, знижки, валюти
+        price_block = soup.find('div', class_='product__price')
+        
+        discount = True if price_block.find('span', class_='product__price--old') else False
+        price = float(price_block.text.strip().split()[0])
+        currency = price_block.text.strip().split()[-1].replace('.','')
+        
+        
+        # print('--->' + product_name + '<---')
+        # print('--->' + str(status) + '<---')
+        # print('--->' + str(discount) + '<---')
+        # print('--->' + currency + '<---')
+        # print('--->' + str(price) + '<---')
+        
+        
+        price_obj = Price(price=price, currency=currency, discount=discount, status=status, unit_of_measure=None)
+        product_obj = Product(product_name=product_name, store_name='Staff', url=url, tg_id=tg_id)
+        
+        # print(f'{product_obj}\n\n{price_obj}')
+        
+        # async with SessionLocal() as session:
+        #     session.add(product)
+        #     await session.commit()
+            
+        return product_obj, price_obj
 
 
 async def get_parse_staff(url: str, tg_id: int) -> Tuple['Product', 'Price'] | None:
     async with aiohttp.ClientSession() as session:
         parser = StaffParser(session)
-        res = await parser.parse(url, tg_id)
-        return res
+        html_content = await parser.fetch_page(url)
+        
+        if html_content:
+            res = await parser.parse(html_content, url, tg_id)
+            return res
+        else: 
+            return None
 
 
-async def run_parsers(urls: List[str]) -> None:
+async def get_parsed_changes_staff(html_content, url: str, tg_id: int) -> Tuple['Product', 'Price']:
+    async with aiohttp.ClientSession() as session:
+        parser = StaffParser(session)
+        return await parser.parse(html_content, url, tg_id)
+
+
+async def run_parsers(url: List[str]) -> None:
     """Тест роботи парсира"""
     async with aiohttp.ClientSession() as session:
         parser = StaffParser(session)
-        res = await parser.parse(urls, tg_id=123)
+        html_content = await parser.fetch_page(url)
+        
+        if html_content:
+            res = await parser.parse(html_content, url, tg_id=123)
+            print(res)
+        else: 
+            return None
         print(res)
         
 
