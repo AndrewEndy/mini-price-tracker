f72f196e94c19f02b9aac6638264a5d26b438449
Andrew <andrijpastuh@gmail.com>
2025-01-15 23:32:14 +0200

    Change epicentr_parser
    Change the structure of parsing, now everything works more separately from each other
    
    Add a new function get_parsed_changes_epicentr for correct operation of asynchronous parsing

--- app/parser/epicentr_parser.py
+++ app/parser/epicentr_parser.py
@@ -9,85 +9,96 @@ from app.db import SessionLocal
 
 
 class EpicentrParser(BaseParser):
-    async def parse(self, url: str, tg_id: int) -> Tuple['Product', 'Price'] | None:
-        html_content = await self.fetch_page(url)
+    async def parse(self, html_content, url: str, tg_id: int) -> Tuple['Product', 'Price'] | None:
+        soup = BeautifulSoup(html_content, 'html.parser')
         
-        if html_content:
-            soup = BeautifulSoup(html_content, 'html.parser')
+        product_name = soup.find('h1', class_='_aql9TB _7TBdaN _GuJjCI').text.strip()
+        
+        # Пошук статуса товара
+        if soup.find('div', class_='_A7y+id'):
+            status = soup.find('div', class_='_A7y+id').text.strip()
+        else: 
+            status = None
             
-            product_name = soup.find('h1', class_='_aql9TB _7TBdaN _GuJjCI').text.strip()
             
-            # Пошук статуса товара
-            if soup.find('div', class_='_A7y+id'):
-                status = soup.find('div', class_='_A7y+id').text.strip()
-            else: 
-                status = None
-                
-                
-            if status != 'Немає в наявності':
-                
-                # Пошук акції
-                if soup.find('s', class_='_Yf4yHx'):
-                    discount = True
-                else:
-                    discount = False
-                    
-                    
-                if discount:
-                    price_block = soup.find('div', class_='_Al-5uY _gqV+xi',).text.strip()
-                else:
-                    price_block =   soup.find('div', class_='_Al-5uY').text.strip()
-                    
-                # Пошук валюти
-                currency = 'грн' if price_block.split('/')[0][-1] == '₴' else price_block.split('/')[0].split()[-1]
-                
-                # Пошук одиниці виміру
-                unit_of_measure = price_block.split('/')[-1]
-                
-                # Пошук ціни
-                price = float(''.join(price_block.split('/')[0].split()[:-1]))
-                
+        if status != 'Немає в наявності':
+            
+            # Пошук акції
+            if soup.find('s', class_='_Yf4yHx'):
+                discount = True
             else:
                 discount = False
-                price = 0.0
-                currency = None
-                unit_of_measure = None
-            
-            # print('--->' + product_name + '<---')
-            # print('--->' + status + '<---')
-            # print('--->' , discount , '<---')
-            # print('--->' + currency + '<---')
-            # print('--->' + unit_of_measure + '<---')
-            # print('--->' , price , '<---')
-            
-            
-            price_obj = Price(price=price, currency=currency, discount=discount, status=status, unit_of_measure=unit_of_measure)
-            product_obj = Product(product_name=product_name, store_name='Епіцентр', url=url, tg_id=tg_id)
-            
-            # print(f'{product_obj}\n\n{price_obj}')
-            
-            # async with SessionLocal() as session:
-            #     session.add(product)
-            #     await session.commit()
                 
-            return product_obj, price_obj
+                
+            if discount:
+                price_block = soup.find('div', class_='_Al-5uY _gqV+xi',).text.strip()
+            else:
+                price_block =   soup.find('div', class_='_Al-5uY').text.strip()
+                
+            # Пошук валюти
+            currency = 'грн' if price_block.split('/')[0][-1] == '₴' else price_block.split('/')[0].split()[-1]
+            
+            # Пошук одиниці виміру
+            unit_of_measure = price_block.split('/')[-1]
+            
+            # Пошук ціни
+            price = float(''.join(price_block.split('/')[0].split()[:-1]))
+            
         else:
-            return None
+            discount = False
+            price = 0.0
+            currency = None
+            unit_of_measure = None
+        
+        # print('--->' + product_name + '<---')
+        # print('--->' + status + '<---')
+        # print('--->' , discount , '<---')
+        # print('--->' + currency + '<---')
+        # print('--->' + unit_of_measure + '<---')
+        # print('--->' , price , '<---')
+        
+        
+        price_obj = Price(price=price, currency=currency, discount=discount, status=status, unit_of_measure=unit_of_measure)
+        product_obj = Product(product_name=product_name, store_name='Епіцентр', url=url, tg_id=tg_id)
+        
+        # print(f'{product_obj}\n\n{price_obj}')
+        
+        # async with SessionLocal() as session:
+        #     session.add(product)
+        #     await session.commit()
+            
+        return product_obj, price_obj
 
 
 async def get_parse_epicentr(url: str, tg_id: int) -> Tuple['Product', 'Price'] | None:
     async with aiohttp.ClientSession() as session:
         parser = EpicentrParser(session)
-        res = await parser.parse(url, tg_id)
-        return res
+        html_content = await parser.fetch_page(url)
+        
+        if html_content:
+            res = await parser.parse(html_content, url, tg_id)
+            return res
+        else: 
+            return None
 
 
-async def run_parsers(urls: List[str]) -> None:
+async def get_parsed_changes_epicentr(html_content, url: str, tg_id: int) -> Tuple['Product', 'Price']:
+    async with aiohttp.ClientSession() as session:
+        parser = EpicentrParser(session)
+        return await parser.parse(html_content, url, tg_id)
+
+
+async def run_parsers(url: List[str]) -> None:
     """Тест роботи парсира"""
     async with aiohttp.ClientSession() as session:
         parser = EpicentrParser(session)
-        res = await parser.parse(urls, tg_id=123)
-        print(res)
+        html_content = await parser.fetch_page(url)
+        
+        if html_content:
+            res = await parser.parse(html_content, url, tg_id=123)
+            print(res)
+        else: 
+            return None
         
 
 if __name__ == "__main__":
